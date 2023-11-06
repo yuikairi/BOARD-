4b511c633526d406ba622582d909578d9f14f63b
yuikairi <yhmtc@icloud.com>
2023-11-06 13:22:45 +0900

    boards削除

--- boards/rating.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Rating(models.Model):
-    # レーティングのフィールドを定義する
-    # 例: value = models.IntegerField()
-    
-    
-    def __str__(self):
-        return f"Rating: {self.value}"
