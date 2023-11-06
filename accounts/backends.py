6a4c6246743348fcddb5362788c7e5bc3bf649ab
yuikairi <yhmtc@icloud.com>
2023-11-06 13:18:16 +0900

    accounts削除

--- accounts/backends.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.backends import ModelBackend
-
-class MyCustomBackend(ModelBackend):
-    def authenticate(self, request, username=None, password=None, **kwargs):
-        
-        pass
--- accounts/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class CustomUser(AbstractUser):
-   profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
