2789efb2b9b2c9334b216aa9ba34c78cd0ea4f38
yuikairi <yhmtc@icloud.com>
2023-11-06 17:39:13 +0900

    post_create&boards_views.py

--- boards/urls.py
+++ boards/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import CreatePostView,PostDeleteView
+from .views import CreatePostView,PostDeleteView,ranking_view
 
 app_name = 'boards'
 urlpatterns = [
@@ -11,7 +11,7 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('search_school/', views.search_school, name='search_school'),
     path('school/<int:school_id>/', views.school_detail, name='school_detail'),
-    path('ranking', views.ranking, name='ranking'), 
+    path('ranking', views.ranking_view, name='ranking'), 
     path('boards/post_done/<int:school_id>/<int:city_id>/', views.PostSuccessView.as_view(),name='post_done'),
     path('post_delete/<int:pk>/', views.PostDeleteView.as_view(), name='post_delete'),
     path('delete_success/', views.DeleteSuccessView.as_view(), name='delete_success'),
